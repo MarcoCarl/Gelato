95e860ff3099c624c24a2e6574e7836365d3fbfd
Marco Carletti <marcocarletti46@gmail.com>
2022-09-16 23:27:44 +0200

    Implements code gelato

---
+++ gelato.py
@@ -0,0 +1,9 @@
+gelato=input('Che gusto vuoi tra vanilla, cioccolato e crema \n')
+costo=''
+if gelato=='vanilla':
+    costo='4$'
+elif gelato=='cioccolato' :
+    costo='5$'
+elif gelato=='crema':
+    costo='2$'
+print('il tuo costo è di'+' '+costo)
